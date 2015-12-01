c39f81b8a12b98283581f64d50f1fc41188c7c95
felipe@dellPrecision <felipe.celer@gmail.com>
2015-12-01 19:22:07 +0000

    Fixed the right OpenGL driver selection on windows

--- src/Apps/Controller/main.cpp
+++ src/Apps/Controller/main.cpp
@@ -5,15 +5,36 @@
 #include "MainWindow.h"
 
 // QT libraries
-#include <QApplication>
+#include <QtWidgets/QApplication>
+#include <QtGui/QSurfaceFormat>
 
 int main( int argc, char *argv[] )
 {
-    QApplication a( argc, argv );
+    QApplication app( argc, argv );
+
+    /// Selecting the Desktop Driver instead the software one
+    /// @see https://forum.qt.io/topic/51574/opengl-dynamic-selected-driver/3
+    app.setAttribute(Qt::AA_UseDesktopOpenGL);
+
+    bool testingNativeDriver = app.testAttribute(Qt::AA_UseDesktopOpenGL);
+
+    if (!testingNativeDriver)
+    {
+    	std::cout << "Neither, your system do not have the hardware requirement (OpenGL 4.3+) " << std::endl
+    			  << "nor the driver are not load properly" << std::endl;
+    	return 0;
+    }
+
+    	    QSurfaceFormat format;
+    	    format.setDepthBufferSize(24);
+    	    format.setVersion(4, 3);
+    	    format.setProfile(QSurfaceFormat::CompatibilityProfile);
+    	    format.setRenderableType(QSurfaceFormat::OpenGL);
+    	    QSurfaceFormat::setDefaultFormat(format);
 
     MainWindow w;
     w.show();
 
-    return a.exec();
+    return app.exec();
 }
 
