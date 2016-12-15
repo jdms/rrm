5a8382cae05c983c661fcadf7de0aab8a8f52692
Windows UCalgary <felipe.celer@gmail.com>
2016-12-14 17:23:21 -0700

    Few aesthetics modificaton on the region item

--- src/Apps/Sketching/RegionItem.cpp
+++ src/Apps/Sketching/RegionItem.cpp
@@ -20,13 +20,22 @@ namespace RRM
 		setFlag(ItemIsMovable);
 		setFlag(ItemSendsGeometryChanges);
 		setCacheMode(DeviceCoordinateCache);
-		setZValue(-1);
+		setZValue(1);
 	}
 
 	QRectF RegionItem::boundingRect() const
 	{
-		qreal adjust = 2;
-		return QRectF(-10 - adjust, -10 - adjust, 23 + adjust, 23 + adjust);
+		qreal adjust = 5;
+
+		QPolygon poly;
+		poly << QPoint(0, -20);
+		poly << QPoint(25, 0);
+		poly << QPoint(0, 20);
+		poly << QPoint(-25, 0); 
+
+		return poly.boundingRect();
+
+		//return QRectF(-10 - adjust, -10 - adjust, 23 + adjust, 23 + adjust);
 	}
 
 	QPainterPath RegionItem::shape() const
@@ -36,27 +45,47 @@ namespace RRM
 		return path;
 	}
 
-	void RegionItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *)
+	void RegionItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget * widget)
 	{
-		painter->setPen(Qt::NoPen);
-		painter->setBrush(Qt::darkGray);
-		painter->drawEllipse(-7, -7, 20, 20);
+		painter->setRenderHints(QPainter::SmoothPixmapTransform | QPainter::Antialiasing, true);
+		painter->setRenderHints(QPainter::TextAntialiasing, true);
+		// Revert QGraphicsView Scale to the default coordinate System.
+		painter->scale(1, -1);
+		// Shadow
+		//painter->setPen(Qt::NoPen);
+		//painter->setBrush(Qt::darkGray);
+		//painter->drawEllipse(-7, -7, 20, 20);
 
 		QRadialGradient gradient(-3, -3, 10);
 		if (option->state & QStyle::State_Sunken) {
 			gradient.setCenter(3, 3);
 			gradient.setFocalPoint(3, 3);
-			gradient.setColorAt(1, QColor(Qt::yellow).light(120));
-			gradient.setColorAt(0, QColor(Qt::darkYellow).light(120));
+			gradient.setColorAt(1, QColor(Qt::white).light(80));
+			gradient.setColorAt(0, QColor(Qt::white).light(120));
 		}
 		else {
-			gradient.setColorAt(0, Qt::yellow);
-			gradient.setColorAt(1, Qt::darkYellow);
+			gradient.setColorAt(0, Qt::white);
+			gradient.setColorAt(1, QColor(Qt::white).light(80));
 		}
-		painter->setBrush(gradient);
 
-		painter->setPen(QPen(Qt::black, 0));
-		painter->drawEllipse(-10, -10, 20, 20);
+		painter->setBrush(gradient);
+		painter->setPen(QPen(Qt::black, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+
+		QPolygon poly;
+		poly << QPoint(0, -12);
+		poly << QPoint(10, 0);
+		poly << QPoint(0, 12);
+		poly << QPoint(-10, 0);
+
+		painter->drawPolygon(poly);
+
+		//painter->drawEllipse(-0, -0, 20, 20);
+
+		painter->setFont(QFont("Arial", 8, QFont::Bold));
+
+		// I have used the .adjusted() to make the text centralized on the diamond.
+		painter->drawText(poly.boundingRect().adjusted(poly.boundingRect().width()*(-0.05), poly.boundingRect().height()*(-0.1), 0, 0), Qt::AlignCenter, QObject::tr("9"));
+		
 	}
 
 	QVariant RegionItem::itemChange(GraphicsItemChange change, const QVariant &value)
