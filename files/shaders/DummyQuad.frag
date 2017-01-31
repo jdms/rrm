2c1a515ead74e7d53014b0bff4863a4e38c58411
Clarissa Coda Marques Machado Silva <ccodamar@cs.ucalgary.ca>
2017-01-31 12:30:39 -0700

    Adding licenses to files and libraries folders

--- files/shaders/DummyQuad.frag
+++ files/shaders/DummyQuad.frag
@@ -1,5 +1,25 @@
 #version 410 core
 
+/** @license
+ * RRM - Rapid Reservoir Modeling Project
+ * Copyright (C) 2015
+ * UofC - University of Calgary
+ *
+ * This file is part of RRM Software.
+ *
+ * RRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RRM.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 out vec4 out_Color;
 
--- src/Core/Geometry/PolygonalCurve/PolygonalCurve2D.cpp
+++ src/Core/Geometry/PolygonalCurve/PolygonalCurve2D.cpp
@@ -1,8 +1,22 @@
-/*
- * PolygonalCurve2D.cpp
+/** @license
+ * RRM - Rapid Reservoir Modeling Project
+ * Copyright (C) 2015
+ * UofC - University of Calgary
  *
- *  Created on: Aug 23, 2015
- *      Author: felipe
+ * This file is part of RRM Software.
+ *
+ * RRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RRM.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "PolygonalCurve2D.hpp"
