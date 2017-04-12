f153ed30314d5f4873d44cf61f4564c8be94c815
Julio Daniel <jd.silva@ucalgary.ca>
2017-04-12 16:46:02 -0600

    Integrate the stratmod library
    This commit updates cmake list files and the rules processor class to
    make use of the stratmod library.  The code compiles and runs.

--- src/Apps/MainWindow/ExtrusionRulesProcessor.hpp
+++ src/Apps/MainWindow/ExtrusionRulesProcessor.hpp
@@ -30,10 +30,7 @@
 
 #include "Core/Geometry/PolygonalCurve/PolygonalCurve2D.hpp"
 
-#include "./libs/PlanInLib/srules.hpp"
-#include "./libs/PlanInLib/planar_surface.hpp"
-#include "./libs/PlanInLib/core.hpp"
-
+#include "stratigraphy_modeller.hpp" 
 
 
 
@@ -62,6 +59,16 @@ namespace RRM
             BOUNDED
         };
 
+        ExtrusionRulesProcessor();
+        ~ExtrusionRulesProcessor() = default;
+
+        ExtrusionRulesProcessor( const ExtrusionRulesProcessor& ) = delete;
+        ExtrusionRulesProcessor& operator=( const ExtrusionRulesProcessor& ) = delete;
+
+        ExtrusionRulesProcessor( const ExtrusionRulesProcessor&& ) = delete;
+        ExtrusionRulesProcessor& operator=( const ExtrusionRulesProcessor&& ) = delete;
+
+        /* DEPRECATED */
         bool isInitialized() const;
 
         std::vector<size_t> getActiveSurfaces(); 
@@ -74,7 +81,7 @@ namespace RRM
         // be used as input either for an `defineAbove()` or a `defineBelow()`.
         // Return: true if at least one elegible surface was found.
         //
-        bool requestDefineRegion( std::vector<size_t> &eligible_surfaces );
+        /* bool requestDefineRegion( std::vector<size_t> &eligible_surfaces ); */
 
         bool requestDefineAbove( std::vector<size_t> &eligible_surfaces );
         bool requestDefineBelow( std::vector<size_t> &eligible_surfaces );
@@ -84,9 +91,6 @@ namespace RRM
 
         /* Change the model's properties */
 
-        size_t getMaxDiscretizationLevel() const;  
-
-        bool setMaxDiscretizationLevel( size_t level ); 
 
         bool requestChangeDiscretizationLevel( size_t level_width, size_t level_height ); 
 
@@ -193,28 +197,9 @@ namespace RRM
         size_t getLegacyMeshes( std::vector<double> &points, std::vector<size_t> &nu, std::vector<size_t> &nv, size_t num_extrusion_steps = 1 );
 
     private:
-        class SRules container_;
+        StratigraphyModeller modeller; 
 
-        // New type names for clarity
-        using ControllerSurfaceIndex = size_t; 
-        using ContainerSurfaceIndex = size_t; 
-
-        // The history
-        std::map<ControllerSurfaceIndex, ContainerSurfaceIndex> dictionary_;
-        std::vector<ControllerSurfaceIndex> inserted_surfaces_indices_; 
-
-        // Model properties
-        Point3 origin_, lenght_; 
-
-        size_t numI_, numJ_; 
-        size_t max_discretization_level_; 
-        size_t level_I_, level_J_;
-
-        // Class status
-        bool initialized_ = false;
-        bool got_origin_ = false; 
-        bool got_lenght_ = false;
-
+        double origin_z_;
 
         enum class InternalState : int {
             UNDEFINED = -1,
@@ -225,49 +210,14 @@ namespace RRM
             REMOVE_BELOW_INTERSECTION
         };
 
-        struct StateDescriptor
-        {
-            State state_ = State::UNDEFINED;
-            bool bounded_above_ = false;
-            ControllerSurfaceIndex upper_boundary_ = 0;
-            bool bounded_below_ = false;
-            ControllerSurfaceIndex lower_boundary_ = 0;
-            std::vector<ControllerSurfaceIndex> truncate_lower_boundary_ = std::vector<ControllerSurfaceIndex>();
-            std::vector<ControllerSurfaceIndex> truncate_upper_boundary_ = std::vector<ControllerSurfaceIndex>();
-        };
-        StateDescriptor current_;
+        State current_state_;
 
-        // Undo stack
-        std::vector<PlanarSurface::Ptr> undoed_surfaces_stack_; 
-        std::vector<size_t> undoed_surfaces_indices_;
-        std::vector<StateDescriptor> undoed_states_;
-        std::vector<StateDescriptor> past_states_;
-
-        // Helper methods
-        void requestChangeDiscretization( size_t discretization_WIDTH, size_t discretization_DEPTH ); 
-
-        bool getSurfaceIndex( const size_t controller_index, size_t &container_index ) const;
-
-        bool getSurfaceID( const size_t controller_index, PlanarSurface::SurfaceId &surface_id );
 
         bool addSurface( 
-                const std::vector<Point3> &curve, 
+                const std::vector<double> &curve, 
                 size_t index, 
                 std::vector<size_t> lb_id = std::vector<size_t>(), 
                 std::vector<size_t> ub_id = std::vector<size_t>() 
-            );
-
-        void registerState(ControllerSurfaceIndex given_index, ContainerSurfaceIndex index); 
-
-        bool enforceDefineRegion(); 
-
-        bool parseTruncateSurfaces( std::vector<size_t> &truncate_lower_boundary, std::vector<size_t> &truncate_upper_boundary );
-
-        bool commitSurface( 
-                PlanarSurface::Ptr &sptr, 
-                size_t index, 
-                std::vector<size_t> lb_id, 
-                std::vector<size_t> ub_id
             );
     };
 
@@ -285,16 +235,13 @@ namespace RRM
             return false; 
         }
 
-        std::vector<Point3> curve( in_curve.size() ); 
-        Point3 p;
+        std::vector<double> curve; //( 3*in_curve.size() ); 
 
-        for ( size_t i = 0; i < curve.size(); ++i ) 
+        for ( size_t i = 0; i < in_curve.size(); ++i ) 
         {
-            p.x = in_curve[i].x();
-            p.z = in_curve[i].y();
-            p.y = origin_.depth;
-
-            curve[i] = p;
+            curve.push_back(in_curve[i].x());
+            curve.push_back(in_curve[i].y());
+            curve.push_back(origin_z_);
         }
 
         return addSurface( curve, id, lower_bound_id, upper_bound_id ); 
@@ -317,58 +264,21 @@ namespace RRM
     template<typename VertexList, typename FaceList>
     bool ExtrusionRulesProcessor::getMesh(size_t surface_index, VertexList &vlist, FaceList &flist)
     {
-        size_t index; 
-        if ( getSurfaceIndex(surface_index, index) == false )
-        {
-            return false; 
-        }
+        return modeller.getMesh(surface_index, vlist, flist); 
 
-        return container_[index]->getMesh(vlist, flist); 
     }
 
     template<typename VertexList, typename FaceList>
     bool ExtrusionRulesProcessor::getInteriorPCL( VertexList &vlist, FaceList &flist )
     {
+        _RP_UNUSED(vlist); _RP_UNUSED(flist);
         return false;
     }
 
     template<typename VertexList, typename EdgeList>
     bool ExtrusionRulesProcessor::getCurve(size_t surface_id, VertexList &vlist, EdgeList &elist)
     {
-        size_t index; 
-        if ( getSurfaceIndex(surface_id, index) == false )
-        {
-            return false; 
-        }
-
-        PlanarSurface::Ptr sptr( container_[index] ); 
-
-        vlist.resize( 2 * sptr->getNumX() );
-        elist.clear(); 
-
-        double height, previous_height; 
-        bool status, previous_status; 
-
-        sptr->getHeight(0, 0, height);
-        vlist[0] = origin_.x;
-        vlist[1] = height;
-
-        for ( size_t i = 1; i < sptr->getNumX(); ++i )
-        {
-            status          = sptr->getHeight(    i, 0, height);
-            previous_status = sptr->getHeight(i - 1, 0, previous_height);
-
-            vlist[2*i + 0] = origin_.x + (float)(i) * lenght_.x / ( (float)(sptr->getNumX() - 1) );
-            vlist[2*i + 1] = height;
-
-            if ( status || previous_status )
-            {
-                elist.push_back(i - 1);
-                elist.push_back(i);
-            }
-        }
-
-        return true;
+        return modeller.getCrossSectionDepth(surface_id, vlist, elist, 0);
     }
 
 
