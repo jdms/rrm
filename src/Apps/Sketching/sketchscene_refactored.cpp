1bf9d7e97f2c00762401c025d614eb6b33bcbe90
Clarissa Coda Marques Machado Silva <ccodamar@cs.ucalgary.ca>
2017-05-26 18:13:44 -0600

    Cleaning sketch operation up

--- src/Apps/Sketching/sketchscene_refactored.cpp
+++ src/Apps/Sketching/sketchscene_refactored.cpp
@@ -18,7 +18,7 @@ void SketchScene_Refactored::addVolume( Volume* const& vol )
 
 void SketchScene_Refactored::startSketch( const QPointF& p )
 {
-    if( sketch != nullptr ) return;
+    if( isValidSketch() == true ) return;
 
     sketch = new InputSketch();
     sketch->create( p );
@@ -28,9 +28,43 @@ void SketchScene_Refactored::startSketch( const QPointF& p )
 
 void SketchScene_Refactored::clearSketch()
 {
-    if( sketch == nullptr ) return;
+    if( isValidSketch() == true ) return;
 
     removeItem( sketch );
     delete sketch;
     sketch = nullptr;
+}
+
+
+void SketchScene_Refactored::finishSketch()
+{
+
+    if( isValidSketch() == false ) return;
+    if( sketch->isEmpty() == true ) return;
+
+    Curve2D curve;
+    if( acceptSketch( curve ) == true  )
+    {
+        Curve2D cpy_curve;
+        curve.douglasPeuckerSimplify( cpy_curve, 1.0 );
+        emit curveAccepted( cpy_curve );
+    }
+
+    clearSketch();
+
+}
+
+void SketchScene_Refactored::acceptSketch( Curve2D& curve )
+{
+
+}
+
+
+
+
+
+bool SketchScene_Refactored::isValidSketch()
+{
+    if ( sketch == nullptr ) return false;
+    return true;
 }
