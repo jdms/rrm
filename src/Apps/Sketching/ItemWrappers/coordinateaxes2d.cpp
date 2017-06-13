da516c398e2981f8775b753010aa0257a9ea3481
Clarissa Coda Marques Machado Silva <ccodamar@cs.ucalgary.ca>
2017-06-13 16:54:35 -0600

    Changing 2d axes style

--- src/Apps/Sketching/ItemWrappers/coordinateaxes2d.cpp
+++ src/Apps/Sketching/ItemWrappers/coordinateaxes2d.cpp
@@ -6,50 +6,72 @@
 
 CoordinateAxes2d::CoordinateAxes2d()
 {
-    axisy_length = 10;
-    axisx_length = 10;
+    axisy_length = 50;
+    axisx_length = 50;
 }
 
 
-void CoordinateAxes2d::setAxisXLenght( double l ){}
-void CoordinateAxes2d::setAxisYLenght( double l ){}
+void CoordinateAxes2d::setAxisXLenght( double l )
+{
+    axisx_length = l;
+    update();
+}
+
+void CoordinateAxes2d::setAxisYLenght( double l )
+{
+    axisy_length = l;
+    update();
+}
 
 
  void CoordinateAxes2d::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
  {
+     drawAxisX( painter);
+     drawAxisY( painter);
+ }
+
+
+ void CoordinateAxes2d::drawAxisX( QPainter* p )
+ {
+
+
+     QPointF tail = QPointF( -15, -15 );
+
+     int discX = 10;
+     double stepX = static_cast< double >( axisx_length/ discX );
+
 
      QPen pen;
-     pen.setColor( Qt::black );
-     pen.setWidth( 3 );
+     pen.setWidth( 1 );
+     pen.setColor( Qt::red );
+     p->setPen( pen );
+     p->drawLine( tail.x(), tail.y(), tail.x() + axisx_length, tail.y() );
 
-     QPointF tail = QPointF( -2, -2 );
+     for( int i = 0; i < discX; ++i )
+     {
+         p->drawLine( tail.x() + i*stepX , tail.y() - 3, tail.x() + i*stepX , tail.y() + 3 );
+     }
 
+ }
 
-     QPointF head_axisx = QPointF( 10, -2 );
+ void CoordinateAxes2d::drawAxisY( QPainter* p )
+ {
 
-     QPointF upper_arrow_x = QPointF( head_axisx.x() - 0.1*axisx_length, head_axisx.y() + 0.1*axisx_length*
-                               std::sin( 30*PI/180 ) );
-     QPointF bottom_arrow_x = QPointF( head_axisx.x() - 0.1*axisx_length, head_axisx.y() - 0.1*axisx_length*
-                                std::sin( 30*PI/180 ) );
+     QPointF tail = QPointF( -15, -15 );
 
-     painter->drawLine( tail.x(), tail.y(), tail.x() + axisx_length, tail.y() );
-     painter->drawLine( upper_arrow_x.x(), upper_arrow_x.y(), head_axisx.x(), head_axisx.y() );
-     painter->drawLine( bottom_arrow_x.x(), bottom_arrow_x.y(), head_axisx.x(), head_axisx.y() );
-
-
-
-
-     QPointF head_axisy = QPointF( -2, 10 );
-
-     QPointF upper_arrow_y = QPointF( head_axisy.x() - 0.1*axisy_length*std::sin( 30*PI/180 ),
-                               head_axisy.y() - 0.1*axisy_length );
-     QPointF bottom_arrow_y = QPointF( head_axisy.x() + 0.1*axisy_length*std::sin( 30*PI/180 ),
-                                head_axisy.y() - 0.1*axisy_length );
+     int discY = 10;
+     double stepY = static_cast< double >( axisy_length/ discY );
 
 
-     painter->drawLine( tail.x(), tail.y(), tail.x(), tail.y()+ axisy_length );
-     painter->drawLine( upper_arrow_y.x(), upper_arrow_y.y(), head_axisy.x(), head_axisy.y() );
-     painter->drawLine( bottom_arrow_y.x(), bottom_arrow_y.y(), head_axisy.x(), head_axisy.y() );
+     QPen pen;
+     pen.setWidth( 1 );
+     pen.setColor( Qt::blue );
+     p->setPen( pen );
+     p->drawLine( tail.x(), tail.y(), tail.x(), tail.y()+ axisy_length );
 
+     for( int i = 0; i < discY; ++i )
+     {
+         p->drawLine( tail.x() - 3 , tail.y() + i*stepY, tail.x() + 3 , tail.y() + i*stepY );
+     }
 
  }
