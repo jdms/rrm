553be85d3558fe34ac320af96fa746e6983f3ffe
Clarissa Silva <Clarissa@CURUPIRA.cs.ucalgary.ca>
2017-10-05 18:16:35 -0600

    Testing multiples canvas creation

--- src/apps/sketching/sketchwindow.cpp
+++ src/apps/sketching/sketchwindow.cpp
@@ -2,10 +2,16 @@
 
 SketchWindow::SketchWindow( QWidget* parent ): QMainWindow( parent )
 {
+    createWindow();
 }
 
 
 void SketchWindow::createWindow()
 {
+    cs = new CanvasStack();
+    setCentralWidget( cs );
 
+    cs->addElement( 0, new QGraphicsView() );
+    cs->addElement( 1, new QGraphicsView() );
+    cs->addElement( 2, new QGraphicsView() );
 }
