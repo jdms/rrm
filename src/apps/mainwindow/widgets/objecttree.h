a0ed6ae1af4e8c810616131c04dca60c6c9d8ad2
Clarissa Silva <Clarissa@CURUPIRA.cs.ucalgary.ca>
2017-10-23 16:59:55 -0600

    Setting headers as constants variables

--- src/apps/mainwindow/widgets/objecttree.h
+++ src/apps/mainwindow/widgets/objecttree.h
@@ -10,6 +10,7 @@ class ObjectTree: public QTreeWidget
 {
     Q_OBJECT
 
+
     public:
 
         ObjectTree( QWidget* parent = 0 );
@@ -19,6 +20,9 @@ class ObjectTree: public QTreeWidget
     private:
 
         const int COLUMNS_NUMBER = 3;
+        const int COLUMN_STATUS = 0;
+        const int COLUMN_NAME = 1;
+        const int COLUMN_COLOR = 2;
 };
 
 #endif // OBJECTTREE_H
