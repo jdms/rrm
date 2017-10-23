a14bae2bf288393f4ba9d094de2b985d548f485b
Clarissa Silva <Clarissa@CURUPIRA.cs.ucalgary.ca>
2017-10-23 16:59:55 -0600

    Setting headers as constants variables

--- src/apps/mainwindow/widgets/objecttree.cpp
+++ src/apps/mainwindow/widgets/objecttree.cpp
@@ -6,14 +6,14 @@ ObjectTree::ObjectTree( QWidget *parent )
     setColumnCount( COLUMNS_NUMBER );
 
     ObjectTreeItem* test = new ObjectTreeItem();
-    test->setCheckState( 0, Qt::Checked );
-    test->setText( 1, QString( "Volume" ) );
+    test->setCheckState( COLUMN_STATUS, Qt::Checked );
+    test->setText( COLUMN_NAME, QString( "Volume" ) );
 
     addTopLevelItem( test );
 
     ObjectTreeItem* test1 = new ObjectTreeItem();
-    test1->setCheckState( 0, Qt::Checked );
-    test1->setText( 1, QString( "Surface" ) );
+    test1->setCheckState( COLUMN_STATUS, Qt::Checked );
+    test1->setText( COLUMN_NAME, QString( "Surface" ) );
 
     test->addChild( test1 );
 
