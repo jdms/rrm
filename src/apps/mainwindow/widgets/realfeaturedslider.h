caee43bac61b997e4d9d2963b5c8901e09ba5cde
Clarissa Silva <clarissacoda@gmail.com>
2017-10-26 13:00:43 -0600

    Adding surfaces back to scene3d

--- src/apps/mainwindow/widgets/realfeaturedslider.h
+++ src/apps/mainwindow/widgets/realfeaturedslider.h
@@ -22,51 +22,49 @@ class RealFeaturedSlider: public Slider
 
     signals:
 
-
-
-            void sliderMoved( const double& value_ );
-            void markValue( const double& value );
-            void unmarkValue( double value );
-            void hightlightValue( double value );
+        void sliderMoved( const double& value_ );
+        void markValue( const double& value );
+        void unmarkValue( double value );
+        void hightlightValue( double value );
 
 
     public slots:
 
 
-            void addMarker( double value );
-            void removeMarker( double value );
+        void addMarker( double value );
+        void removeMarker( double value );
 
 
-            void setRange( double min_, double max_ );
-            void setDiscretization( int disc_ );
+        void setRange( double min_, double max_ );
+        void setDiscretization( int disc_ );
 
-            void setValue( double value_ );
-            double getDoubleValue( int index_ ) const;
-            double getValue() const ;
+        void setValue( double value_ );
+        double getDoubleValue( int index_ ) const;
+        double getValue() const ;
 
 
-            int getIndex( double value_ ) const ;
+        int getIndex( double value_ ) const ;
 
-            void increaseValue();
-            void decreaseValue();
+        void increaseValue();
+        void decreaseValue();
 
 
     protected slots:
 
-            void moveInDouble( int index_ );
+        void moveInDouble( int index_ );
 
 
 
     protected:
 
-            void updateStep();
+        void updateStep();
 
 
-            void hightlightMarker( int pos_ );
-            bool isValidMarker( int value );
-            void updateMarkerPositions();
-            void saveMarkerPosition( int value, int slider_length, int slider_min, int slider_max );
-            void getSubControlDimensions( int& slider_length, int& slider_min, int& slider_max ) const ;
+        void hightlightMarker( int pos_ );
+        bool isValidMarker( int value );
+        void updateMarkerPositions();
+        void saveMarkerPosition( int value, int slider_length, int slider_min, int slider_max );
+        void getSubControlDimensions( int& slider_length, int& slider_min, int& slider_max ) const ;
 
 
 
