53d86b8f7f50059cb7f27b95ae57938be4d62e57
Clarissa Silva <Clarissa@CURUPIRA.cs.ucalgary.ca>
2017-11-16 16:53:34 -0700

    Adding application to clean the interface

--- src/apps/mainwindow/main.cpp
+++ src/apps/mainwindow/main.cpp
@@ -38,45 +38,6 @@ std::size_t Object::number_of_objects = 0;
 std::size_t CrossSection::number_of_csections;
 
 
-enum class AppsCommands{ NEW, SAVE, LOAD, UNDO, REDO };
-enum class CommonCommands{ ADD, REMOVE, SET, GET };
-enum class CommonProperties{ NAME, COLOR, INDEX, CURRENT, VISIBLE, SELECTABLE, SELECTED, ACTIVE};
-enum class CommonObjects{ VOLUME, STRATIGRAPHY, CROSS_SECTION, REGION };
-
-struct Color
-{
-    int r = 255;
-    int b = 0;
-    int g = 0;
-};
-
-
-template < typename TI, typename TO = void > struct FunctionType
-{
-    using method = std::function< TO( TI args ) >;
-    using method_void = std::function< void( TI args ) >;
-    using method2 = std::function< TO( CommonObjects obj_, TI args ) >;
-};
-
-
-template < typename TI, typename TI1, typename TO = void > struct FunctionType1
-{
-    using method = std::function< TO( CommonObjects obj_, TI args, TI1 args1 ) >;
-    using method_void = std::function< void( CommonObjects obj_, TI args, TI1 args1 ) >;
-};
-
-
-
-template < class TI, typename TI1, class TO = void >
-TO run_method( CommonObjects obj_, TI args1, TI1 args )
-{
-
-}
-
-
-using method_void = std::function< void(void) >;
-
-
 int main( int argc, char *argv[] )
 {
 
@@ -105,71 +66,71 @@ int main( int argc, char *argv[] )
     w.show();
 
 
-    std::unordered_map< AppsCommands, method_void > runs;
-    runs[ AppsCommands::NEW ] = [](void)->void{};
-    runs[ AppsCommands::SAVE ] = [](void)->void{};
-    runs[ AppsCommands::LOAD ] = [](void)->void{};
-    runs[ AppsCommands::UNDO ] = [](void)->void{};
-    runs[ AppsCommands::REDO ] = [](void)->void{};
+//    std::unordered_map< AppsCommands, method_void > runs;
+//    runs[ AppsCommands::NEW ] = [](void)->void{};
+//    runs[ AppsCommands::SAVE ] = [](void)->void{};
+//    runs[ AppsCommands::LOAD ] = [](void)->void{};
+//    runs[ AppsCommands::UNDO ] = [](void)->void{};
+//    runs[ AppsCommands::REDO ] = [](void)->void{};
 
 
 
-    using method1_sizet = FunctionType< std::size_t >::method_void;
-    using method1_double = FunctionType< double >::method_void;
-    std::unordered_map< CommonObjects, method1_sizet > adds, remove;
-    std::unordered_map< CommonObjects, method1_double > adds_double, remove_double;
+//    using method1_sizet = FunctionType< std::size_t >::method_void;
+//    using method1_double = FunctionType< double >::method_void;
+//    std::unordered_map< CommonObjects, method1_sizet > adds, remove;
+//    std::unordered_map< CommonObjects, method1_double > adds_double, remove_double;
 
 
 
-    adds[ CommonObjects::VOLUME ] = []( std::size_t )->void{};
-    adds[ CommonObjects::STRATIGRAPHY ] = []( std::size_t )->void{};
-    adds[ CommonObjects::REGION ] = []( std::size_t )->void{};
-    adds_double[ CommonObjects::CROSS_SECTION ] = []( double )->void{};
+//    adds[ CommonObjects::VOLUME ] = []( std::size_t )->void{};
+//    adds[ CommonObjects::STRATIGRAPHY ] = []( std::size_t )->void{};
+//    adds[ CommonObjects::REGION ] = []( std::size_t )->void{};
+//    adds_double[ CommonObjects::CROSS_SECTION ] = []( double )->void{};
 
 
-    remove[ CommonObjects::VOLUME ] = []( std::size_t )->void{};
-    remove[ CommonObjects::STRATIGRAPHY ] = []( std::size_t )->void{};
-    remove[ CommonObjects::REGION ] = []( std::size_t )->void{};
-    remove_double[ CommonObjects::CROSS_SECTION ] = []( double )->void{};
+//    remove[ CommonObjects::VOLUME ] = []( std::size_t )->void{};
+//    remove[ CommonObjects::STRATIGRAPHY ] = []( std::size_t )->void{};
+//    remove[ CommonObjects::REGION ] = []( std::size_t )->void{};
+//    remove_double[ CommonObjects::CROSS_SECTION ] = []( double )->void{};
 
 
-    using method_string = FunctionType1< std::size_t, std::string >::method_void;
-    using method_bool = FunctionType1< std::size_t, bool >::method_void;
-    using method_color = FunctionType1< std::size_t, Color >::method;
+//    using method_string = FunctionType1< std::size_t, std::string >::method_void;
+//    using method_bool = FunctionType1< std::size_t, bool >::method_void;
+//    using method_color = FunctionType1< std::size_t, Color >::method;
 
-    std::unordered_map< CommonProperties, method_string > setters_string;
-    std::unordered_map< CommonProperties, method_bool > setters_bool;
-    std::unordered_map< CommonProperties, method_color > setters_color;
-
-
-
-    setters_string[ CommonProperties::NAME ] = []( CommonObjects obj_, std::size_t, std::string )->void{};
-    setters_color[ CommonProperties::COLOR ] = []( CommonObjects obj_, std::size_t, Color )->void{};
-    setters_bool[ CommonProperties::VISIBLE ] = []( CommonObjects obj_, std::size_t, bool )->void{};
-    setters_bool[ CommonProperties::SELECTABLE ] = []( CommonObjects obj_, std::size_t, bool )->void{};
-    setters_bool[ CommonProperties::SELECTED] = []( CommonObjects obj_, std::size_t, bool )->void{};
-    setters_bool[ CommonProperties::ACTIVE ] = []( CommonObjects obj_, std::size_t, bool )->void{};
-    setters_bool[ CommonProperties::CURRENT ] = []( CommonObjects obj_, std::size_t, bool )->void{};
-    setters_bool[ CommonProperties::INDEX ] = []( CommonObjects obj_, std::size_t, bool )->void{};
+//    std::unordered_map< CommonProperties, method_string > setters_string;
+//    std::unordered_map< CommonProperties, method_bool > setters_bool;
+//    std::unordered_map< CommonProperties, method_color > setters_color;
 
 
 
-    using method_string1 = FunctionType< std::size_t, std::string >::method2;
-    using method_bool1 = FunctionType< std::size_t, bool >::method2;
-    using method_color1 = FunctionType< std::size_t, Color >::method2;
+//    setters_string[ CommonProperties::NAME ] = []( CommonObjects obj_, std::size_t, std::string )->void{};
+//    setters_color[ CommonProperties::COLOR ] = []( CommonObjects obj_, std::size_t, Color )->void{};
+//    setters_bool[ CommonProperties::VISIBLE ] = []( CommonObjects obj_, std::size_t, bool )->void{};
+//    setters_bool[ CommonProperties::SELECTABLE ] = []( CommonObjects obj_, std::size_t, bool )->void{};
+//    setters_bool[ CommonProperties::SELECTED] = []( CommonObjects obj_, std::size_t, bool )->void{};
+//    setters_bool[ CommonProperties::ACTIVE ] = []( CommonObjects obj_, std::size_t, bool )->void{};
+//    setters_bool[ CommonProperties::CURRENT ] = []( CommonObjects obj_, std::size_t, bool )->void{};
+//    setters_bool[ CommonProperties::INDEX ] = []( CommonObjects obj_, std::size_t, bool )->void{};
 
-    std::unordered_map< CommonProperties, method_string1 > getters_string;
-    std::unordered_map< CommonProperties, method_bool1 > getters_bool;
-    std::unordered_map< CommonProperties, method_color1 > getters_color;
 
-    getters_string[ CommonProperties::NAME ] = []( CommonObjects obj_, std::size_t )->std::string{ return std::string(); };
-    getters_color[ CommonProperties::COLOR ] = []( CommonObjects obj_, std::size_t )->Color{ return Color(); };
-    getters_bool[ CommonProperties::VISIBLE ] = []( CommonObjects obj_, std::size_t )->bool{ return true;};
-    getters_bool[ CommonProperties::SELECTABLE ] = []( CommonObjects obj_, std::size_t )->bool{ return true;};
-    getters_bool[ CommonProperties::SELECTED]  = []( CommonObjects obj_, std::size_t )->bool{ return true;};
-    getters_bool[ CommonProperties::ACTIVE ] = []( CommonObjects obj_, std::size_t )->bool{ return true;};
-    getters_bool[ CommonProperties::CURRENT ] = []( CommonObjects obj_, std::size_t )->bool{ return true; };
-    getters_bool[ CommonProperties::INDEX ] = []( CommonObjects obj_, std::size_t )->bool{return true; };
+
+//    using method_string1 = FunctionType< std::size_t, std::string >::method2;
+//    using method_bool1 = FunctionType< std::size_t, bool >::method2;
+//    using method_color1 = FunctionType< std::size_t, Color >::method2;
+
+//    std::unordered_map< CommonProperties, method_string1 > getters_string;
+//    std::unordered_map< CommonProperties, method_bool1 > getters_bool;
+//    std::unordered_map< CommonProperties, method_color1 > getters_color;
+
+//    getters_string[ CommonProperties::NAME ] = []( CommonObjects obj_, std::size_t )->std::string{ return std::string(); };
+//    getters_color[ CommonProperties::COLOR ] = []( CommonObjects obj_, std::size_t )->Color{ return Color(); };
+//    getters_bool[ CommonProperties::VISIBLE ] = []( CommonObjects obj_, std::size_t )->bool{ return true;};
+//    getters_bool[ CommonProperties::SELECTABLE ] = []( CommonObjects obj_, std::size_t )->bool{ return true;};
+//    getters_bool[ CommonProperties::SELECTED]  = []( CommonObjects obj_, std::size_t )->bool{ return true;};
+//    getters_bool[ CommonProperties::ACTIVE ] = []( CommonObjects obj_, std::size_t )->bool{ return true;};
+//    getters_bool[ CommonProperties::CURRENT ] = []( CommonObjects obj_, std::size_t )->bool{ return true; };
+//    getters_bool[ CommonProperties::INDEX ] = []( CommonObjects obj_, std::size_t )->bool{return true; };
 
 
 
--- src/apps/mainwindow/mainwindow.cpp
+++ src/apps/mainwindow/mainwindow.cpp
@@ -1,10 +1,5 @@
-#include "mainwindow.h"
-#include "3dview/canvas3d.h"
-#include "sketching/sketchwindow.h"
-#include "widgets/realfeaturedslider.h"
-#include "widgets/objecttree.h"
-#include "widgets/pages_stack.h"
 
+#include "mainwindow.h"
 
 #include <QtWidgets>
 #include <QActionGroup>
@@ -12,7 +7,6 @@
 #include <QDir>
 #include <QFileInfo>
 #include <QDesktopServices>
-
 
 
 MainWindow::MainWindow( QWidget *parent ) : QMainWindow( parent )
@@ -24,8 +18,8 @@ MainWindow::MainWindow( QWidget *parent ) : QMainWindow( parent )
 
     show();
 
-    createController();
-    setupController();
+//    createController();
+//    setupController();
     run_app();
 }
 
@@ -58,9 +52,10 @@ void MainWindow::createWindow()
 {
     createMainInterface();
     createSidebar();
-    createBottombar();
-    createToolbar();
     createSketchingWindow();
+
+    createToolbar();
+
 }
 
 
@@ -86,14 +81,6 @@ void MainWindow::createMainInterface()
     central_widget->setLayout( hb_central_widget );
     setCentralWidget( central_widget );
 
-
-    connect( this, &MainWindow::updateVolume, [=](){ double width_ = 0, height_ = 0, length_ = 0;
-                                                     controller->getVolumeDimensions( width_, height_, length_ );
-                                                     sl_depth_csection->setRange( 0, length_ ); } );
-
-    connect( sl_depth_csection, &RealFeaturedSlider::sliderMoved, [=]( const double& value_ )
-                                                                  { controller->setCurrentCrossSection( value_ ); } );
-
 }
 
 
@@ -101,22 +88,16 @@ void MainWindow::createMainInterface()
 void MainWindow::createToolbar()
 {
 
-    ac_clear = new QAction( "New", this );
-    connect( ac_clear, &QAction::triggered, this, &MainWindow::clear );
-
     QAction* ac_save = new QAction( "Save", this );
     QAction* ac_load = new QAction( "Load", this );
 
+    ac_clear = new QAction( "New", this );
+
     ac_undo = new QAction( "Undo", this );
-    connect( ac_undo, &QAction::triggered, [=](){ controller->undo(); checkUndoRedo(); } );
-
     ac_redo = new QAction( "Redo", this );
-    connect( ac_redo, &QAction::triggered, [=](){ controller->redo(); checkUndoRedo(); } );
-
 
     ac_sketch_above = new QAction( "SA", this );
     ac_sketch_above->setCheckable( true );
-
 
     ac_sketch_below = new QAction( "SB", this );
     ac_sketch_below->setCheckable( true );
@@ -143,26 +124,7 @@ void MainWindow::createToolbar()
     ag_rules->addAction( ac_remove_below_int );
 
 
-    connect( ag_rules, &QActionGroup::triggered, [=]( QAction* action_ ) {
-                                                 if( action_ == ac_remove_above )
-                                                     controller->setRemoveAbove();
-                                                 else if( action_ == ac_remove_above_int )
-                                                     controller->setRemoveAboveIntersection();
-                                                 else if( action_ == ac_remove_below )
-                                                     controller->setRemoveBelow();
-                                                 else if( action_ == ac_remove_below_int )
-                                                     controller->setRemoveBelowIntersection();
-    } );
-
-
-    connect( this, &MainWindow::resetMenus, [=](){ ac_undo->setEnabled( false ); ac_redo->setEnabled( false );
-                                                   ac_remove_above->setChecked( true );  ac_sketch_above->setCheckable( false );
-                                                   ac_sketch_below->setCheckable( false ); } );
-
-
-
     QAction* ac_output_volume = new QAction( "Get Regions", this );
-    connect( ac_output_volume, &QAction::triggered, [=](){ controller->getOutputVolume(); } );
 
     QToolBar* tb_mainwindow = new QToolBar();
     tb_mainwindow->addAction( ac_clear );
@@ -190,76 +152,12 @@ void MainWindow::createSidebar()
     dw_object_tree->setWidget( object_tree );
     addDockWidget( Qt::LeftDockWidgetArea, dw_object_tree );
 
-    connect( object_tree, &ObjectTree::setVolumeVisible, [=]( std::size_t index_, bool status_ )
-                                                         { controller->setVolumeVisibility( status_ ); } );
-
-
-    connect( object_tree, &ObjectTree::setObjectVisible, [=]( std::size_t index_, bool status_ )
-                                                         { controller->setObjectVisibility( index_, status_ ); } );
-
-
-    connect( object_tree, &ObjectTree::setObjectName, [=]( std::size_t index_, const std::string& name_ )
-                                                       { controller->setObjectName( index_, name_ ); } );
-
-
-
-
-}
-
-
-
-void MainWindow::createBottombar()
-{
 
     object_properties = new PagesStack();
     dw_object_properties = new QDockWidget( "" );
     dw_object_properties->setAllowedAreas( Qt::LeftDockWidgetArea );
     dw_object_properties->setWidget( object_properties );
     addDockWidget( Qt::LeftDockWidgetArea, dw_object_properties );
-
-    connect( this, &MainWindow::defineVolumeDimensions, object_properties, &PagesStack::changeVolumeSize );
-
-    connect( object_properties, &PagesStack::heightVolumeChanged, [=]( double height_ ){
-                                                                      double w = object_properties->getWidth();
-                                                                      double d = object_properties->getLenght();
-                                                                      controller->setVolumeDimensions( w, height_, d );
-                                                                      emit updateVolume(); } );
-    connect( object_properties, &PagesStack::widthVolumeChanged, [=]( double width_ ){
-                                                                      double h = object_properties->getHeight();
-                                                                      double d = object_properties->getLenght();
-                                                                      controller->setVolumeDimensions( width_, h, d );
-                                                                      emit updateVolume(); } );
-
-    connect( object_properties, &PagesStack::depthVolumeChanged, [=]( double lenght_ ){
-                                                                      double w = object_properties->getWidth();
-                                                                      double h = object_properties->getHeight();
-                                                                      controller->setVolumeDimensions( w, h, lenght_ );
-                                                                      emit updateVolume(); } );
-
-    connect( object_tree, &ObjectTree::itemClicked, [=]( QTreeWidgetItem* item_, int col_ )
-    {
-        ObjectTreeItem* obj_item_ = static_cast< ObjectTreeItem* >( item_ );
-        if( obj_item_->getType() == ObjectTreeItem::Type::VOLUME )
-            object_properties->setCurrentIndex( 0 );
-        else
-            object_properties->setCurrentIndex( 1 );
-
-        std::string text_ = controller->getObjectInformation( obj_item_->getIndex() );
-        object_properties->loadObjectInformation( text_ );
-    } );
-
-
-    connect( object_properties, &PagesStack::saveText, [=]( const QString& text_ )
-    {
-        QList< QTreeWidgetItem* > list = object_tree->selectedItems();
-        if (list.empty() == true ) return;
-        ObjectTreeItem* item_ = static_cast< ObjectTreeItem* >( list[ 0 ] );
-        std::cout << item_->getIndex() << std::endl << std::flush;
-        std::size_t index_ = item_->getIndex();
-        controller->saveObjectInformation( index_, text_.toStdString() );
-    } );
-
-
 
 }
 
@@ -274,76 +172,13 @@ void MainWindow::createSketchingWindow()
     dw_sketchwindow->setWidget( sketch_window );
     addDockWidget( Qt::BottomDockWidgetArea, dw_sketchwindow );
 
-    connect( this, &MainWindow::defineMainCrossSection, [=]( double v ){ controller->setMainCrossSection( CrossSection::Direction::Z, v );
-                                                                         sketch_window->setMainCanvas( controller->getCrossSection( v ) );
-                                                                         sketch_window->setCurrentCrossSection( v );
-                                                                         /*controller->addTopViewCrossSection();
-                                                                         sketch_topview_window->setMainCanvas( controller->getTopViewCrossSection() );*/ } );
+    sketch_topview_window = new SketchWindow();
+    dw_topview_window = new QDockWidget( "Top-View" );
+    dw_topview_window->setAllowedAreas( Qt::AllDockWidgetAreas );
+    dw_topview_window->setWidget( sketch_topview_window );
+    dw_topview_window->setVisible( true );
+    addDockWidget( Qt::BottomDockWidgetArea, dw_topview_window );
 
-
-
-    connect( ac_sketch_below, &QAction::triggered, [=]( bool status_ ) {
-                                                 bool enabled_ = controller->enableCreateBelow( status_ );
-                                                 if( ( status_ == false ) && ( enabled_ == true ) )
-                                                 {
-                                                     std::cout << "Stop failed, so keep turned it on" << std::endl << std::flush;
-                                                     ac_sketch_below->setChecked( true );
-                                                 }
-                                                 if( ( status_ == true ) && ( enabled_ == false ) )
-                                                 {
-                                                     std::cout << "Request failed, so keep turned it off" << std::endl << std::flush;
-                                                     ac_sketch_below->setChecked( false );
-                                                 }
-
-                                                 if( ( status_ == true ) && ( enabled_ == true ) )
-                                                 {
-                                                     std::cout << "Request accepted, get the selectable objects" << std::endl << std::flush;
-                                                     std::cout << "and mark them as selectable" << std::endl << std::flush;
-                                                     sketch_window->setModeSelecting();
-                                                     sketch_window->updateCanvas();
-
-                                                 }
-                                                 if( ( status_ == false ) && ( enabled_ == false ) )
-                                                 {
-                                                     std::cout << "Stop accepted, get the selectable objects" << std::endl << std::flush;
-                                                     std::cout << "and mark them as non-selectable" << std::endl << std::flush;
-                                                     sketch_window->setModeSketching();
-                                                     sketch_window->updateCanvas();
-                                                 }
-    } );
-
-
-
-//    sketch_topview_window = new SketchWindow();
-//    dw_topview_window = new QDockWidget( "Top-View" );
-//    dw_topview_window->setAllowedAreas( Qt::AllDockWidgetAreas );
-//    dw_topview_window->setWidget( sketch_topview_window );
-//    dw_topview_window->setVisible( true );
-//    addDockWidget( Qt::BottomDockWidgetArea, dw_topview_window );
-
-
-//    connect( this, &MainWindow::updateVolume, sketch_topview_window, &SketchWindow::updateVolumes );
-//    connect( this, &MainWindow::addObject, sketch_topview_window, &SketchWindow::addTrajectory );
-//    connect( this, &MainWindow::updateObject, sketch_topview_window, &SketchWindow::updateTrajectory );
-//    connect( this, &MainWindow::updateObjects, sketch_topview_window, &SketchWindow::updateTrajectories );
-//    connect( this, &MainWindow::addCrossSection, sketch_topview_window, &SketchWindow::addCrossSection );
-
-
-//    connect( sketch_topview_window, &SketchWindow::acceptCurve, [=]( const PolyCurve& curve_ ){ bool status_ = controller->addObjectTrajectory( curve_ );
-//                                                                                        if( status_ == false ) return;
-//                                                                                        emit updateObjects(); } );
-
-
-//    connect( sketch_topview_window, &SketchWindow::commitObject, [=](){ bool status_ = controller->createObjectSurface();
-//                                                                        if( status_ == false ) return;
-
-//                                                                        controller->addObject();
-//                                                                        object_properties->setEnabledVolumeResize( controller->isVolumeResizable() );
-
-//                                                                        emit addObject( controller->getCurrentObject() );
-//                                                                        emit setUpColor();
-
-//                                                                        checkUndoRedo(); } );
 
 }
 
@@ -382,8 +217,12 @@ void MainWindow::setupSlider()
 
 void MainWindow::run_app()
 {
-    controller->init();
-    setupSlider();
+
+    app = new RRMApplication( this );
+    app->init();
+
+//    controller->init();
+//    setupSlider();
 
 
 }
