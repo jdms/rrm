0d9b5e1439b74bb692cc4f560f0bc374bdedcd2c
Clarissa Silva <Clarissa@CURUPIRA.cs.ucalgary.ca>
2018-04-19 16:17:19 -0600

    Adding license to files

--- src/apps/simulator/well_module/well_canvas.hpp
+++ src/apps/simulator/well_module/well_canvas.hpp
@@ -1,9 +1,24 @@
-/*
- * Canvas.hpp
+/** @license
+ * RRM - Rapid Reservoir Modeling Project
+ * Copyright (C) 2015
+ * UofC - University of Calgary
  *
- *  Created on: Sep 13, 2016
- *      Author: felipe
+ * This file is part of RRM Software.
+ *
+ * RRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RRM.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 #ifndef _CANVAS_HPP_
 #define _CANVAS_HPP_
