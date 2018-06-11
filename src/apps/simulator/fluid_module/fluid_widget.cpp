649901a54d92cecf843f9ed5622c20ca50592d58
Windows UCalgary <felipe.celer@gmail.com>
2018-06-11 15:14:31 -0600

    ..

--- src/apps/simulator/fluid_module/fluid_widget.cpp
+++ src/apps/simulator/fluid_module/fluid_widget.cpp
@@ -1,9 +1,25 @@
-/*
- * WellWidget.cpp
+/** @license
+ * RRM - Rapid Reservoir Modeling Project
+ * Copyright (C) 2015
+ * UofC - University of Calgary
  *
- *  Created on: Apr 16, 2017
- *      Author: felipe
+ * This file is part of RRM Software.
+ *
+ * RRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RRM.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
 
 #include "fluid_widget.hpp"
 
@@ -58,7 +74,7 @@ namespace RRM
 			multi_phase_type_ = MultiPhaseType(int(fluid_data["multi_phase_type_"].toDouble()));
 		}
 		
-		QVector<QString> attributes({ "oil_viscosity", "oil_density", "bo", "water_viscosity", "water_density", "bw", "fwl" });
+		std::vector<QString> attributes = { "oil_viscosity", "oil_density", "bo", "water_viscosity", "water_density", "bw", "fwl" });
 
 		for (std::size_t i = 0; i < attributes.size(); i++)
 		{
