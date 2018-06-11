7b0ad05dc28c43237dcc3a7f227c495664cff5f6
Windows UCalgary <felipe.celer@gmail.com>
2018-06-11 15:14:31 -0600

    ..

--- src/core/models/object.h
+++ src/core/models/object.h
@@ -1,3 +1,26 @@
+
+/** @license
+ * RRM - Rapid Reservoir Modeling Project
+ * Copyright (C) 2015
+ * UofC - University of Calgary
+ *
+ * This file is part of RRM Software.
+ *
+ * RRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RRM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #ifndef OBJECT_H
 #define OBJECT_H
 
