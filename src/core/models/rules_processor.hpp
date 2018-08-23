6896dfc6ada578b7ad5149e27f7fd61a493a75f1
Julio Daniel <jd.silva@ucalgary.ca>
2018-08-23 15:07:56 -0600

    Fix compilations issues
    This is in preparation to provide general preserve[Above,Below,Region]
    functionality to allow structural rules

--- src/core/models/rules_processor.hpp
+++ src/core/models/rules_processor.hpp
@@ -117,6 +117,15 @@
             /* Query or modify the automatum state */
 
 
+            /////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////
+            //  All old define[Above,Below] methods have become deprecated as 
+            //  we move to allow structural rules on models.  
+            //
+            //  Refer to new preserve[Above, Below] methods
+            /////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////
+
             //
             // brief:
             // Define new input region above surface which index is `surface_index`.
@@ -169,6 +178,46 @@
             bool defineAboveIsActive( std::vector<size_t> &boundary_indices );
             bool defineBelowIsActive( std::vector<size_t> &boundary_indices );
             // END: use this instead
+
+            /////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////
+            //
+            // New structure aware preserve[Above, Below]
+            //
+            /////////////////////////////////////////////////////////////////////////////
+
+            void stopPreserveAbove();
+            void stopPreserveBelow();
+            void stopPreserveRegion();
+
+            void preserveAboveIsActive();
+            void preserveBelowIsActive();
+            void preserveRegionIsActive();
+
+            bool requestPreserveRegion( std::vector<double> &point );
+            bool requestPreserveAbove( std::vector<double> &curve_points );
+            bool requestPreserveBelow( std::vector<double> &curve_points );
+
+            bool preserveAbove( std::vector<std::size_t> &lower_model );
+            bool preserveBelow( std::vector<std::size_t> &upper_model );
+
+            bool getModelAboveSurface( std::vector<double> &curve_points, std::vector<size_t> &upper_model );
+            bool getModelBelowSurface( std::vector<double> &curve_points, std::vector<size_t> &lower_model );
+
+            bool getUpperBoundaryMesh( std::vector<float> &vlist, std::vector<size_t> &flist );
+            bool getLowerBoundaryMesh( std::vector<float> &vlist, std::vector<size_t> &flist );
+
+            bool getUpperBoundaryLengthwiseCrossSection( size_t cross_sec, std::vector<float> &vlist, std::vector<size_t> &flist );
+            bool getUpperBoundaryWidthwiseCrossSection( size_t cross_sec, std::vector<float> &vlist, std::vector<size_t> &flist );
+
+            bool getLowerBoundaryLengthwiseCrossSection( size_t cross_sec, std::vector<float> &vlist, std::vector<size_t> &flist );
+            bool getLowerBoundaryWidthwiseCrossSection( size_t cross_sec, std::vector<float> &vlist, std::vector<size_t> &flist );
+
+            /////////////////////////////////////////////////////////////////////////////
+
+
+
 
             void removeAbove();
             void removeAboveIntersection();
