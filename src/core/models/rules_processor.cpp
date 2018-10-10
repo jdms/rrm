1da622ad06fae7a69adbee5e94a84b5a9c4cecaf
Julio Daniel <jd.silva@ucalgary.ca>
2018-10-09 23:00:48 -0600

    Fix colormap

--- src/core/models/rules_processor.cpp
+++ src/core/models/rules_processor.cpp
@@ -70,7 +70,7 @@ void deprecationNotice( std::string deprecated_method, std::string new_method, b
 
 #include "rules_processor.hpp"
 
-#include "colormap/color_map_wrapper.hpp"
+#include "colormap/colormap.hpp"
 //namespace RRM
 //{}
 
@@ -1952,9 +1952,7 @@ bool RulesProcessor::getRegionsForSimulationTetrahedralMesh( const std::vector<d
 
 std::vector<int> RulesProcessor::getRegionsColor( std::size_t numColors )
 {
-    ColorMapWrapper cm;
-
-    return cm.getSpectral(numColors);
+    return Colormap::Spectral(numColors);
 }
 
 bool RulesProcessor::getQuadMesh( std::size_t surface_id, std::vector<double> &points, std::vector<bool> &valid_points, std::size_t &num_width, std::size_t &num_length )
