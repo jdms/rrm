ced18be1d6ea79e6d34964201f88042c5d25fc19
Julio Daniel <jd.silva@ucalgary.ca>
2019-03-18 15:39:39 -0600

    Update the stratmod library

--- libs/stratmod/src/smodeller_primitives.hpp
+++ libs/stratmod/src/smodeller_primitives.hpp
@@ -1,14 +1,14 @@
 #ifndef __SMODELLER_PRIMITIVES_HPP__
 #define __SMODELLER_PRIMITIVES_HPP__
 
-#include "win_dll_export_handler.hpp"
+#include "stratmod_win_dll_export_handler.hpp"
 
 
 
 /* Avoids warning C4251 in VS2013, see macro definition in file
  * 'win_dll_export_handler.hpp' 
  */
-SILENCE_MSVC_WIN_DLL_EXPORT_WARNING
+STRATMOD_SILENCE_MSVC_WIN_DLL_EXPORT_WARNING
 struct SModellerImplementation; 
 
 
